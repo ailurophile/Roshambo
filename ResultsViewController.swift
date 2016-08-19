09acac5c94ad64ddc5335799982c0e0b955d6731
ailurophile <lisalitchfield@aim.com>
2016-08-18 20:23:34 -0700

    Implemented code only presentation of view controller for rock button, code & segue for paper button, and segue only for scissors button.

---
+++ ResultsViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ResultsViewController.swift
+//  Roshambo
+//
+//  Created by Lisa Litchfield on 8/13/16.
+//  Copyright © 2016 Lisa Litchfield. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var results: UILabel!
+
+    @IBOutlet weak var resultsImage: UIImageView!
+    
+    var playerChoice: Roshambo = .Rock
+    override func viewWillAppear(animated: Bool) {
+        playRoshambo()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func playRoshambo(){
+        let computerPick = Int(arc4random_uniform(3))
+        print("Computer picks \(computerPick)")
+        let choice = Roshambo(rawValue: computerPick)!
+        print("Which is \(choice)")
+        print("Player picked \(playerChoice)")
+       
+        switch (choice, playerChoice){
+        case (.Rock, .Rock), (.Paper, .Paper), (.Scissors, .Scissors):
+            results.text = "It's a tie!"
+            self.resultsImage.image = UIImage(named: "itsATie")
+        case (.Rock, .Paper):
+            results.text = "Paper covers rock. You win!"
+            self.resultsImage.image = UIImage(named: "PaperCoversRock")
+        case(.Rock, .Scissors):
+            results.text = "Rock crushes scissors. You lose!"
+            self.resultsImage.image = UIImage(named: "RockCrushesScissors")
+        case (.Paper, .Rock):
+            results.text = "Paper covers rock. You lose!"
+            self.resultsImage.image = UIImage(named: "PaperCoversRock")
+        case (.Paper, .Scissors):
+            results.text = "Scissors cut paper. You win!"
+            self.resultsImage.image = UIImage(named: "ScissorsCutPaper")
+        case (.Scissors, .Rock):
+            results.text = "Rock crushes scissors.  You win!"
+            self.resultsImage.image = UIImage(named: "RockCrushesScissors")
+        case (.Scissors, .Paper):
+            results.text = "Scissors cut paper. You lose!"
+            self.resultsImage.image = UIImage(named: "ScissorsCutPaper")
+ 
+            
+        }
+ 
+    }
+
+    @IBAction func playAgain(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
---
+++ ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Roshambo
+//
+//  Created by Lisa Litchfield on 8/12/16.
+//  Copyright © 2016 Lisa Litchfield. All rights reserved.
+//
+
+import UIKit
+enum Roshambo:Int {
+    case Rock = 0, Paper, Scissors
+}
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func makeSelection(sender: UIButton){
+        var controller: ResultsViewController
+        let choice: Roshambo = (Roshambo(rawValue: sender.tag)!)
+        print("button raw value: \(sender.tag) which means \(choice) selected")
+        switch choice {
+        case .Rock:
+            // code only presentation
+            controller = self.storyboard?.instantiateViewControllerWithIdentifier("ResultsViewController") as! ResultsViewController
+            controller.playerChoice = choice
+            presentViewController(controller, animated: true, completion: nil)
+        case .Paper:
+            //Code & segue presentation
+            self.performSegueWithIdentifier("playGame", sender: self)
+            
+        default:
+            print("This should never happen!")
+        
+        
+        }
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let controller = segue.destinationViewController as! ResultsViewController
+        print(segue.identifier)
+        if segue.identifier == "playGame" {
+            controller.playerChoice = .Paper
+            
+        }
+        if segue.identifier == "scissorsButton"{
+            
+          controller.playerChoice = (Roshambo(rawValue: sender!.tag)!)
+        }
+        
+    }
+
+}
+
